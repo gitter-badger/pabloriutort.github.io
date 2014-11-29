19179e79b8474c550a58224892e601c1eecbadfe
Pablo <pablo.riutort@gmail.com>
2014-11-29 22:42:08 +0100

    add target blank to all links

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+//This adds "target = "_blank" to all links
+var links = document.getElementsByTagName('a');
+var len = links.length;
+
+for(var i=0; i<len; i++)
+{
+   links[i].target = "_blank";
+}
